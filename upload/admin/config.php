c3388ed1fc6511d80452ce4c6ccaca5b1fba4675
zilgrg <zilgrg@gmail.com>
2013-11-14 16:44:11 +0200

    config.pgp and admin/config.php copied from webserver
    config.pgp and admin/config.php copied from webserver

---
+++ upload/admin/config.php
@@ -0,0 +1,34 @@
+<?php
+// BACKEND CONFIG
+
+// HTTP
+define('HTTP_SERVER', 'http://yama.lt/admin/');
+define('HTTP_CATALOG', 'http://yama.lt/');
+define('HTTP_IMAGE', 'http://yama.lt/image/');
+define('HTTP_ADMIN', 'http://yama.lt/admin/');
+
+// HTTPS
+define('HTTPS_SERVER', HTTP_SERVER);
+define('HTTPS_IMAGE', HTTP_IMAGE);
+
+// DIR
+define('DIR_CATALOG', '/home/yamahace/domains/yama.lt/public_html/catalog/');
+define('DIR_APPLICATION', '/home/yamahace/domains/yama.lt/public_html/admin/');
+define('DIR_SYSTEM', '/home/yamahace/domains/yama.lt/public_html/system/');
+define('DIR_DATABASE', DIR_SYSTEM.'database/');
+define('DIR_LANGUAGE', DIR_APPLICATION.'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION.'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM.'config/');
+define('DIR_IMAGE', '/home/yamahace/domains/yama.lt/public_html/image/');
+define('DIR_CACHE', DIR_SYSTEM.'cache/');
+define('DIR_DOWNLOAD', '/home/yamahace/domains/yama.lt/public_html/download/');
+define('DIR_LOGS', DIR_SYSTEM.'logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'yamahace_oc3');
+define('DB_PASSWORD', 'N|QaU|i0P&@@8');
+define('DB_DATABASE', 'yamahace_oc3');
+define('DB_PREFIX', 'oc_');
+?>
