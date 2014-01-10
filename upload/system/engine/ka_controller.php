a7c20acbc7d35b771afb92088d60e4b027209bd8
zilgrg <zilgrg@gmail.com>
2014-01-11 00:18:16 +0200

    CSV Product Import update from 3.5.0 to 3.5.1  - second attempt
    CSV Product Import update from 3.5.0 to 3.5.1  - second attempt, not all
    files were commited with first commit
    
    https://www.opencart.com/index.php?route=account/download/info&order_id=406445

---
+++ upload/system/engine/ka_controller.php
@@ -0,0 +1,78 @@
+<?php
+/*
+	Project: Ka-Extensions
+	Author : karapuz <support@ka-station.com>
+
+	Version: 2.0 ($Revision: 13 $)
+*/
+
+abstract class KaController extends Controller {
+
+	protected $params;
+
+	function __construct(&$registry) {
+		parent::__construct($registry);
+
+		$class = get_class($this);
+		if (!isset($this->session->data["ka_params_$class"])) {
+			$this->session->data["ka_params_$class"] = array();
+		}
+		$this->params = &$this->session->data["ka_params_$class"];
+	}
+
+	
+	protected function addTopMessage($msg, $type = 'I') {
+		$this->session->data['ka_top_messages'][] = array(
+			'type'    => $type,
+			'content' => $msg
+		);
+	}
+
+	
+	protected function getTopMessages($clear = true) {
+		
+		if (isset($this->session->data['ka_top_messages'])) {
+			$top = $this->session->data['ka_top_messages'];
+		} else {
+			$top = null;
+		}
+
+		if ($clear) {
+			$this->session->data['ka_top_messages'] = null;
+		}
+		return $top;
+	}
+
+	
+	protected function findTemplate($template) {
+		if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . $template)) {
+			$template = $this->config->get('config_template') . $template;
+		} else {
+			$template = 'default' . $template;
+		}
+		
+		return $template;
+	}
+
+			
+	protected function render() {
+
+		$this->data['top_messages'] = $this->getTopMessages();
+		$this->data['ka_top'] = $this->getChild('common/ka_top', $this->data);
+
+		return parent::render();
+	}
+	
+	
+	protected function loadLanguage($language) {
+		if (version_compare(VERSION, '1.5.5', '>=')) {
+			$ret = $this->language->load($language);
+		} else {
+			$ret = $this->load->language($language);
+		}
+		
+		return $ret;
+	}
+	
+}
+?>
