00453af91a609474d165de7cfeaa244757a50caa
zilgrg <zilgrg@gmail.com>
2014-01-21 22:59:36 +0200

    Addresses on Checkout Confirm modification by ZG
    Addresses on Checkout Confirm modification by ZG
    to separate into language files:
    - /catalog/language/english/checkout/checkout.php
    added:
    $_['text_payment_address']   = 'Payment Address';
    $_['text_shipping_address']   = 'Shipping Address';
    
    - /catalog/language/lithuanian/checkout/checkout.php
    added:
    $_['text_payment_address']   = 'Pirkejo adresas';
    $_['text_shipping_address']   = 'Gavejo adresas';

--- upload/catalog/language/lithuanian/checkout/checkout.php
+++ upload/catalog/language/lithuanian/checkout/checkout.php
@@ -28,6 +28,8 @@ $_['text_address_new']               = 'I want to use a new address';
 $_['text_address_existing']          = 'I want to use an existing address';
 $_['text_shipping_method']           = 'Please select the preferred shipping method to use on this order.';
 $_['text_payment_method']            = 'Please select the preferred payment method to use on this order.';
+$_['text_payment_address']           = 'Payment Address';
+$_['text_shipping_address']          = 'Shipping Address';
 $_['text_comments']                  = 'Add Comments About Your Order';
 
 $_['text_recurring_item']            = 'Recurring item';
