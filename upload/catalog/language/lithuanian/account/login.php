89f2e10f582e7c361fa4805e822b87f1088acb03
zilgrg <zilgrg@gmail.com>
2014-03-04 20:40:51 +0200

    Lithuanian language login.php update
    Lithuanian language login.php update

--- upload/catalog/language/lithuanian/account/login.php
+++ upload/catalog/language/lithuanian/account/login.php
@@ -1,15 +1,15 @@
 <?php
 // Heading 
-$_['heading_title']                = 'Yama.lt paskyra';
+$_['heading_title']                = 'Prisijungti arba registruotis';
 
 // Text
 $_['text_account']                 = 'Paskyra';
-$_['text_login']                   = 'Prisijungti';
-$_['text_new_customer']            = 'Esu naujas vartotojas';
-$_['text_register']                = 'Sukurti naują paskyrą';
-$_['text_register_account']        = 'By creating an account you will be able to shop faster, be up to date on an order\'s status, and keep track of the orders you have previously made.';
-$_['text_returning_customer']      = 'Esu registruotas vartotojas';
-$_['text_i_am_returning_customer'] = 'Prisijungti prie esamos paskyros';
+$_['text_login']                   = 'Prisijungti arba registruotis';
+$_['text_new_customer']            = 'Esu naujas vartotojas, registruotis:';
+$_['text_register']                = '';
+$_['text_register_account']        = 'Užsiregistravę galėsite apsipirkti greičiau, matyti užsakymų istoriją, atsisiųsti dokumentus, susikurti skirtingus prekių pristatymo adresus.';
+$_['text_returning_customer']      = 'Esu registruotas vartotojas, prisijungti:';
+$_['text_i_am_returning_customer'] = '';
 $_['text_forgotten']               = 'Priminti slaptažodį';
 
 // Entry
@@ -17,6 +17,6 @@ $_['entry_email']                  = 'Jūsų el. pašto adresas:';
 $_['entry_password']               = 'Slaptažodis:';
 
 // Error
-$_['error_login']                  = 'Dėmesio: El. pašto adresas ir/arba slaptažodis neteisingas.';
+$_['error_login']                  = 'El. pašto adresas ir/arba slaptažodis neteisingas.';
 $_['error_approved']               = 'Dėmesio: Kad galėtumėte prisijungti jūsų paskyra turi būti patvirtinta sistemos administratoriaus. Tai gali užtrukti iki 48h arba prašome susisiekti mūsų pagalbos telefonu.'; 
 ?>
