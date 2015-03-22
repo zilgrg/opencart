cfc7f47a5ed3aa8ead17365557de0beab2882bd3
zilgrg <zilgrg@gmail.com>
2015-03-22 15:58:49 +0200

    remove journal1 stuff
    remove journal1 stuff

--- upload/admin/language/english/module/journal_product_tabs.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Heading
-$_['heading_title']					= '<b>Journal - Product Tabs</b>';
-$_['doc_title']						= 'Product Tabs';
-$_['button_create']					= 'Create New';
-$_['entry_product']					= '<b>Product Name</b>';
-?>
